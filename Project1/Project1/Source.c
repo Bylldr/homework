3d82e786fa09067c157f8efc7f72f6cfaaa6e307
Bylldr <glebov-20052005@yandex.ru>
2023-09-07 21:28:51 +0300

    homework

---
+++ Project1/Project1/Source.c
@@ -0,0 +1,21 @@
+﻿#include<locale.h>
+#include<stdio.h>
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	puts("                           /|");
+	puts("                          / |");
+	puts("                         /  |");
+	puts("                        /   |");
+	puts("                       /    |");
+	puts("                      /-----|");
+	puts("                            |");
+	puts("                 ___________|______________");
+	puts("                 \\____O_____O______O______/");
+	puts("                  \\______O______O________/");
+	puts("                   \\______O______O______/");
+	
+	getchar();
+
+
+}
