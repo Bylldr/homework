684093fc823c7d022fd28cc09f47b1d3252135a7
Bylldr <glebov-20052005@yandex.ru>
2023-09-27 22:12:52 +0300

    123

---
+++ LAB3_TASK3/LAB3_TASK3/Source.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<locale.h>
+int main()
+{
+    setlocale(LC_CTYPE, "RUS");
+    int a;
+    int b;
+    printf("¬ведите первое число: ");
+    scanf("%d", &a);
+    printf("¬ведите второе число: ");
+    scanf("%d", &b);
+    printf("_______________________________\n|%9s|%9s|%9s|\n_______________________________\n|%4d*%4d|%4d+%4d|%4d+%4d|\n________________________________\n|%9d|%9d|%9d|\n________________________________", "a*b", "a+b", "a-b", a, b, a, b, a, b, a * b, a + b, a - b);
+    getchar();
+    system("pause");
+    
+
+}
