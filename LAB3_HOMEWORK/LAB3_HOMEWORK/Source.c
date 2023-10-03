8a67b285a67d3d234b28d2bcec844aa02239d2c9
Bylldr <glebov-20052005@yandex.ru>
2023-10-03 20:39:30 +0300

    123

---
+++ LAB3_HOMEWORK/LAB3_HOMEWORK/Source.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	float price, road;
+	puts("Введите расстояние:");
+	scanf("%f", &road);
+	puts("Введите стоимость бензина:");
+	scanf("%f", &price);
+	printf("Стоимость поездки:%.2f", price * road);
+	getchar();
+
+}
---
+++ LAB4_HOMEWORK/LAB4_HOMEWORK/Source.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<locale.h>
+int main()
+{
+	float x;
+	setlocale(LC_CTYPE, "RUS");
+	puts("Введите расстояние,которое пробежал мальчик:");
+	scanf("%f", &x);
+	printf("Время за которое мальчик пробежал расстояние:%.2f", 4 * 3 * x);
+	getchar();
+
+}
