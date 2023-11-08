41076a67be7e76759d287ea512d4d8845ab1084d
Bylldr <glebov-20052005@yandex.ru>
2023-11-08 21:04:28 +0300

    123

--- LAB15_HM/LAB15_HM/Source.c
+++ LAB15_HM/LAB15_HM/Source.c
@@ -1,9 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
+int zarplata(int a);
+
+int main()
+{
+	int a=0;
+	char finish = '+';
+	setlocale(LC_CTYPE, "RUS");
+	while (finish != '!')
+	{
+		puts("Введите номер сотрудника:");
+		scanf("%d", &a);
+		zarplata(a);
+		printf("Годовая зарплата равна сотрудника № %d: %d\n", a, zarplata(a));
+		puts("Для заверешния введите !\n");
+		getchar();
+		scanf("%c", &finish);
+	}
+	return 0;
+}
 int zarplata(int a)
 {
-	int i, j,sum=0;
+
+	int i, j, sum = 0;
 	int arr[20][12];
 	for (i = 0; i < 20; i++)
 	{
@@ -14,30 +34,21 @@ int zarplata(int a)
 			{
 				arr[i][j] += arr[i - 1][j - 1];
 			}
-			printf("%5d ",arr[i][j]);
+			printf("| %5d | ",arr[i][j]);
 
 		}
 		puts("\n");
 	}
-	for (i=0;i<20;i++)
+	
+	for (i = 0; i < 20; i++)
 	{
 		for (j = 0; j < 12; j++)
 		{
-			if (i==a)
+			if (i == a)
 				sum += arr[i][j];
 
 		}
 
 	}
 	return  sum;
-}
-int main()
-{
-	int a=0;
-	setlocale(LC_CTYPE, "RUS");
-	puts("Введите номер сотрудника:\n\n");
-	scanf("%d", &a);
-	zarplata(a);
-	printf("Годовая зарплата равна сотрудника № %d: %d", a, zarplata(a));
-	return 0;
 }
