ead6b7c9619f79236d84913d8d037d43d607441a
Bylldr <glebov-20052005@yandex.ru>
2023-12-06 19:36:20 +0300

    06.12.23

--- KUrs123/KUrs123/Source.c
+++ KUrs123/KUrs123/Source.c
@@ -1,26 +1,49 @@
+#define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
 #include <locale.h>
+#include <stdbool.h>
 #define _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_DEPRECATE
 #define ROWS 7
 #define COLUMNS 7
 
-char gameBoard[ROWS][COLUMNS];
-char answers[ROWS][COLUMNS] = {
-    {'B','C','E','_','D','_','A'},
-    {'_','D','C','A','_','E','B'},
-    {'C','_','A','B','_','D','E'},
-    {'_','A','B','D','E','C','_'},
-    {'A','B','D','E','C','_','_'},
-    {'E','_','_','C','B','A','D'},
-    {'D','E','_','_','A','B','C'},
+char gameBoard[ROWS][COLUMNS] = {
+    {'_','_','_','_','_','_','_'},
+    {'_','_','_','_','_','_','_'},
+    {'_','_','_','_','_','_','_'},
+    {'_','_','_','_','_','_','_'},
+    {'_','_','_','_','_','_','_'},
+    {'_','_','_','_','_','_','_'},
+    {'_','_','_','_','_','_','_'},
 
 };
+char answers[ROWS][COLUMNS] = {
+       'B','C','E','_','D','_','A',
+       '_','D','C','A','_','E','B',
+       'C','_','A','B','_','D','E',
+       '_','A','B','D','E','C','_',
+       'A','B','D','E','C','_','_',
+       'E','_','_','C','B','A','D',
+       'D','E','_','_','A','B','C',
+   };
+void printrules();
+void createGameBoard();
+void printGameBoard();
+int game(int a);
+void checkRecord(int new_record);
 int main()
 {
     int Complexity;
     int choose;
     char finish = '+';
+    FILE* fp;
+    fp = fopen("game_record.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    fclose(fp);
+    return 0;
+}
+  
     setlocale(LC_CTYPE, "RUS");
     while (finish != '!')
     {
@@ -50,13 +73,6 @@ int main()
 
 }
 
-void printrules();
-void createGameBoard();
-void printGameBoard();
-int game(int a);
-void saveRecord(int record);
-void displayRecord();
-void updateRecord(int newRecord);
 
 
 void createGameBoard()
@@ -74,15 +90,17 @@ void printGameBoard()
 {
     char str1[7] = { 'B','D', 'C', 'A', 'A',' ','D' };//Колонка букв слева от сетки
     char str2[7] = { 'A','B', 'E', 'C', ' ','D',' ' };//Колонка букв справа от сетки
-    printf("          B     E  A  D  E  A\n");
+    printf("        B     E  A  D  E  A\n");
+    printf("        0  1  2  3  4  5  6 \n");
+
     for (int i = 0; i < ROWS; i++)
     {
-        printf("\t%c", str1[i]);
+        printf("   %d  %c",i, str1[i]);
         for (int j = 0; j < ROWS; j++)
         {
             printf("|%c|", gameBoard[i][j]);
             if (j == 6)
-                printf(" %c", str2[i]);
+                printf("%c",  str2[i]);
         }
         puts("\n");
 
@@ -94,6 +112,7 @@ int game(int a)
 
     int x, y, TRY, repeat, save = 0;
     char temp;
+    bool Equa = true;
 
     switch (a)
     {
@@ -102,6 +121,7 @@ int game(int a)
         printf("На данном уровне сложности у вас: %d попыток, жалем удачи ;)\n", TRY);
         while (TRY > 0)
         {
+            printf("%d Осталось попыток");
             printGameBoard();
             puts("Введите номер строки: ");
             scanf_s("%d", &x);
@@ -115,21 +135,41 @@ int game(int a)
                 gameBoard[x][y] = temp;
                 printGameBoard();
                 save += 1;
+                for (int i = 0; i < ROWS; i++)
+                {
+                    for (int j = 0; j < COLUMNS; j++)
+                    {
+                        if (gameBoard[i][j] != answers[i][j])
+                        {
+                            Equa = false;
+                            
+                        }
+                        else
+                            Equa = true;
 
+                    }
+                }
+                if (Equa == true)
+                {
+                    printf("Поздравляем вы выиграли вам понадобилось : %d\n", save);
+                    checkRecord(save);
+                    return 0;
+                }
             }
+
+                
+
             else
             {
-                puts("Неверный ход");
+                puts("Невереный ход");
                 TRY--;
-                printf("Осталось попыток: %d\n\n", TRY);
-
-
+                printf("У вас осталось : %d попыток", TRY);
             }
-
         }
         printf("К сожалению вы проиграли,ваш результат:%d\n", save);
-        updateRecord(save);
+
         puts("\n");
+        checkRecord(save);
         break;
     case 2:
         TRY = 3;
@@ -149,24 +189,44 @@ int game(int a)
                 gameBoard[x][y] = temp;
                 printGameBoard();
                 save += 1;
+                for (int i = 0; i < ROWS; i++)
+                {
+                    for (int j = 0; j < COLUMNS; j++)
+                    {
+                        if (gameBoard[i][j] != answers[i][j])
+                        {
+                            Equa = false;
 
+                        }
+                        else
+                            Equa = true;
+
+                    }
+                }
+                if (Equa == true)
+                {
+                    printf("Поздравляем вы выиграли вам понадобилось : %d\n", save);
+                    checkRecord(save);
+                    return 0;
+                }
             }
             else
             {
-                puts("Неверный ход");
+                puts("Невереный ход");
                 TRY--;
-                printf("Осталось попыток: %d\n\n", TRY);
+                printf("У вас осталось : %d попыток", TRY);
             }
         }
         printf("К сожалению вы проиграли,ваш результат:%d\n", save);
-        updateRecord(save);
         puts("\n");
+        checkRecord(save);
         break;
     case 3:
         TRY = 1;
         printf("На данном уровне сложности у вас: %d попыток, жалем удачи ;)\n", TRY);
         while (TRY > 0)
         {
+
             printGameBoard();
             puts("Введите номер строки: ");
             scanf_s("%d", &x);
@@ -180,93 +240,68 @@ int game(int a)
                 gameBoard[x][y] = temp;
                 printGameBoard();
                 save += 1;
+                for (int i = 0; i < ROWS; i++)
+                {
+                    for (int j = 0; j < COLUMNS; j++)
+                    {
+                        if (gameBoard[i][j] != answers[i][j])
+                        {
+                            Equa = false;
 
+                        }
+                        else
+                            Equa = true;
+
+                    }
+                }
+                if (Equa == true)
+                {
+                    printf("Поздравляем вы выиграли вам понадобилось : %d\n", save);
+                    checkRecord(save);
+                    return 0;
+                }
             }
             else
             {
-                puts("Неверный ход");
+                puts("Невереный ход");
                 TRY--;
-                printf("Осталось попыток: %d\n\n", TRY);
             }
+
 
         }
         printf("К сожалению вы проиграли,ваш результат:%d\n", save);
-        updateRecord(save);
+
         puts("\n");
+        checkRecord(save);
         break;
 
+        }
 
-    }
+        return save;
+  }
 
-    return save;
-}
 void printrules()
 {
     puts("\t\t\t\t||ПРАВИЛА||\nНеобходимо заполнить головоломку буквами, следуя следующим принципам:\n1)В каждом ряду и колонке буква должна встречаться лишь единажды\n2)Некоторые клетки могут быть постыми\n3)Буквы,стоящие на границе сетки,показывают,какая буква должна встретится первой\n");
 
 }
-void saveRecord(int record) {
-    FILE* fp;
-    fp = fopen("game_record.txt", "w");
-    fprintf(fp, "%d", record);
-    fclose(fp);
-}
-void displayRecord() {
+void checkRecord(int new_record) {
     FILE* fp;
     int record;
-    fp = fopen("game_record.txt", "r");
+    fp = fopen("game_record.txt", "r+");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return;
+    }
     fscanf(fp, "%d", &record);
-    printf("Лучший результат: %d\n", record);
-    fclose(fp);
-}
-void updateRecord(int newRecord) {
-    FILE* fp;
-    int currentRecord;
-    fp = fopen("game_record.txt", "r");
-    fscanf(fp, "%d", &currentRecord);
-    fclose(fp);
-    if (newRecord > currentRecord) {
-        saveRecord(newRecord);
-        printf("Поздравляем вы побили рекорд: %d\n", newRecord);
+    if (new_record > record) {
+        fprintf(fp, "%d", new_record);
+        printf("Поздравляем вы побили рекорд: %d\n\n", new_record);
     }
     else {
-        printf("Лучший результат: %d\n", currentRecord);
+        printf("Лучший результат : %d\n\n", record);
     }
+    fclose(fp);
 }
 
 
-
-
-int main()
-{
-    int Complexity;
-    int choose;
-    char finish = '+';
-    setlocale(LC_CTYPE, "RUS");
-    while (finish != '!')
-    {
-        puts("Приветствуем в игре ПЕРВЫЕ ВСТРЕЧНЫЕ, пожалуйства выберите действие:\n1)Ознакомиться с правилами игры\n2)Приступить к игре\n3)Закончить игру");
-        scanf_s("%d", &choose);
-        switch (choose)
-        {
-        case 1:
-            printrules();
-            break;
-        case 2:
-        {
-            createGameBoard();
-            puts("Вырите уровень сложности игры:\n1)Легкий,возможность ошибиться 5 раз\n2)Средний,возможность ошибиться 3 раз\n3)Тяжелый,возможность ошибиться 1 раз\n");
-            scanf_s("%d", &Complexity);
-            game(Complexity);
-            break;
-        }
-        case 3:
-        {
-            finish = '!';
-        }
-        }
-    }
-    puts("Ждем вас снова!");
-    return 0;
-
-}
